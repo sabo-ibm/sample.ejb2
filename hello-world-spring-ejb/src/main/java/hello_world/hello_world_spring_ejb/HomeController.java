f548d613016bba7f56296ff71ae8a6f0e7de5b23
Sabo <sabo@ubm.com>
2024-08-05 09:49:55 -0600

    Fix folder not included for created springmvc5 project hello-world-spring-ejb using ecplise architype

---
+++ hello-world-spring-ejb/src/main/java/hello_world/hello_world_spring_ejb/HomeController.java
@@ -0,0 +1,21 @@
+package hello_world.hello_world_spring_ejb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    /**
+     * Maneja las solicitudes que se le hacen a la raíz del sitio
+     * 
+     * @return un objeto {@link ModelAndView} con la respuesta al cliente
+     */
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public ModelAndView mostrarHome() {
+        return new ModelAndView("home", "mensaje", "Hola (ed)Mundo!");
+    }
+
+}
